e9e4a5f7ed00ab386902df5ab28bc85e13e548d6
Mehdiyunis <mehdiyunis1110@gmail.com>
2023-05-23 16:30:23 +0400

    Fiorello

---
+++ public/main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: true,
+    },
+    loop: true
+});
